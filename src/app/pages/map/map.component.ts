b0dce549e7258175eac79e7517575decfc75ffc7
Ivan Anic4APP <ivan.anic@4app-solution.hr>
2023-01-29 22:12:27 +0100

    initial mock

---
+++ src/app/pages/map/map.component.ts
@@ -0,0 +1,35 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { zgKvartovi } from 'src/app/vars/zagreb_kvartovi'; '../../vars/zagreb_kvartovi'
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements AfterViewInit {
+  private map!: L.Map | L.LayerGroup<any>;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [45.815399, 15.966568],
+      zoom: 13,
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://hr.linkedin.com/in/ivan-ani%C4%87-9a0b27182?trk=people-guest_people_search-card">Ivan Anić</a>'
+    });
+
+    L.geoJson(zgKvartovi).addTo(this.map);
+
+    tiles.addTo(this.map);
+  }
+}
